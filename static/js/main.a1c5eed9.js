45a3ba6ab2a2e778629c575da661d68568b4cf41
dipu-ahmed <121944271+dipu-ahmed@users.noreply.github.com>
2023-02-19 02:25:40 +0600

    Update main.a1c5eed9.js

--- static/js/main.a1c5eed9.js
+++ static/js/main.a1c5eed9.js
@@ -11491,7 +11491,7 @@
                         },
                         type: "button",
                         class:
-                          "font-bold text-3xl font-mono-bg-gradient-to-r from-green-400 p-3 w-[300px] rounded-full to-blue-500 hover:from-pink-500 hover:to-yellow-500 ...",
+                          "font-bold text-3xl font-mono bg-gradient-to-r from-green-400 p-3 w-[300px] rounded-full to-blue-500 hover:from-pink-500 hover:to-yellow-500 ...",
                         children: (0, Ge.jsx)("b", { children:"CLICK HERE" }),
                       }),
                     }),
